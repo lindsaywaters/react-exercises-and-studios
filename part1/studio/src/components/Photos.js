878fe30d6e137aff37c25eda1725093e65d2496f
Lindsay <lwaters0208@gmail.com>
2023-08-03 21:30:07 -0400

    studio 1

---
+++ part1/studio/src/components/Photos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Description.module.css';
+
+function RecipePhoto() {
+    return (
+        <img src="https://www.recipetineats.com/wp-content/uploads/2023/08/Cobb-Salad_0.jpg?resize=650,910" alt="recipe photo" className = {styles.imageUpdates} />
+    )
+}
+
+export default RecipePhoto;
